4fbaf4d92aa8fe6a94a7528b024f28a35ac6a648
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-07-19 14:21:02 -0500

    app intro edited

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,11 @@ export default function Hero() {
   return (
     <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center maw-w-[800px] w-full mx-auto p-4'>
 <div className='flex flex-col gap-4'>
-    <p>IT'S TIME TO GET</p>
-    <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Swoles<span className='text-blue-400'>normus</span></h1>
+    <p>IT'S TIME TO</p>
+    <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>#STAY<span className='text-blue-400'>HARD</span></h1>
     </div>
-    <p className='text-sm md:text-base font-light '>I hereby acknowledgement that I may become < span className='text-blue-400 font medium'>unbelievably swolenormous</span> and accept all risks of becoming the local <span className='text-blue-400 font medium'>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
+    <p className='text-sm md:text-base font-light '>I hereby acknowledgement that I may < span className='text-blue-400 font medium'>want to quit halfway through this routine</span> but by clicking the button below I have promised myself to finishing this workout <span className='text-blue-400 font medium'>OR to sit here and try until I can finish this workout.</span></p>
+    <p className='text-sm md:text-base'>failure will not be an option today</p>
 
 <Button func={() => {
   window.location.href = '/#generate'
